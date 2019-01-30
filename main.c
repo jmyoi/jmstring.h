c7f70186890ad98ada1272d7e24d57e4d3139b81
j2ieu <j2ieu@pm.me>
2019-01-30 08:51:26 -0800

    destructor,  resize, shrink_to_fit, push_back, reserve added

--- main.c
+++ main.c
@@ -3,43 +3,67 @@
 
 int main(int argc, char **argv)
 {
-	string *a = newstr("Hello");
+	string *a = _new("Hello");
+
 	if(a == NULL)
 	{
 		printf("null\n");
 	}
 	else
 	{
-		printf("\n");
+		_assign(a, "HELLO WORLD");
 		printf("string  : %s\n", a->str);
 		printf("c_str   : %s\n", c_str(a));
-		printf("size    : %d\n", size(a));
-		printf("length  : %d\n", length(a));
-		printf("capacity: %d\n", capacity(a));
+		printf("size    : %d\n", _size(a));
+		printf("length  : %d\n", _length(a));
+		printf("capacity: %d\n", _capacity(a));
+		printf("at: %c\n", _at(a, 1));
+		printf("front: %c\n", _front(a));
+		printf("back: %c\n", _back(a));
 		printf("\n");
 
-		assign(a, "HELLO WORLD");
+		_resize(a, 5);
+		printf("resize(5): %s\n", a->str);
+		printf("length  : %d\n", _length(a));
+		printf("capacity: %d\n", _capacity(a));
+		printf("\n");
+
+		_resize(a, 7, "+");
+		printf("resize(7): %s\n", a->str);
+		printf("length  : %d\n", _length(a));
+		printf("capacity: %d\n", _capacity(a));
+		printf("\n");
+
+		_resize(a, 6, "+");
+		printf("resize(6): %s\n", a->str);
+		printf("length  : %d\n", _length(a));
+		printf("capacity: %d\n", _capacity(a));
+		printf("\n");
+
+		_reserve(a, 18);
 		printf("string  : %s\n", a->str);
-		printf("c_str   : %s\n", c_str(a));
-		printf("size    : %d\n", size(a));
-		printf("length  : %d\n", length(a));
-		printf("capacity: %d\n", capacity(a));
+		printf("length  : %d\n", _length(a));
+		printf("capacity: %d\n", _capacity(a));
 		printf("\n");
 
-		assign(a, "Myoi");
+		_shrink_to_fit(a);
 		printf("string  : %s\n", a->str);
-		printf("c_str   : %s\n", c_str(a));
-		printf("size    : %d\n", size(a));
-		printf("length  : %d\n", length(a));
-		printf("capacity: %d\n", capacity(a));
+		printf("length  : %d\n", _length(a));
+		printf("capacity: %d\n", _capacity(a));
 		printf("\n");
 
-		printf("at: %c\n", at(a, 2));
-		printf("front: %c\n", front(a));
-		printf("back: %c\n", back(a));
-		free(a->str);
-		free(a);
+
+		_push_back(a, 'm');
+		_push_back(a, 'y');
+		_push_back(a, 'o');
+		_push_back(a, 'i');
+		printf("string  : %s\n", a->str);
+		printf("length  : %d\n", _length(a));
+		printf("capacity: %d\n", _capacity(a));
 	}
+
+	
+	_free(a);
 
 	return 0;
 }
